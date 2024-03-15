bd71870e69c6ba1c7ee38925350aa53ea94165ea
Jazper Jacob <jazperjacob@Jazpers-MBP.lan>
2024-03-14 22:31:38 -0700

    task_mgr

---
+++ setup.sql
@@ -0,0 +1,32 @@
+
+-- Create database table
+
+CREATE TABLE IF NOT EXISTS task (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(128),
+    summary VARCHAR(256),
+    description TEXT,
+    is_done BOOLEAN DEFAULT 0
+);
+
+-- Create some dummy data to test with:
+INSERT INTO task (
+    name,
+    summary,
+    description
+) VALUES
+(
+    "Wash dishes",
+    "Use dish soap to wash dishes",
+    "Lorem Ipsum (description)"
+),
+(
+    "Walk dog",
+    "Take Fido to the park for a walk",
+    "Make sure Fido gets their excercise"
+),
+(
+    "Buy groceries",
+    "Drive to the store and buy groceries",
+    "We need eggs, bread, milk and ham"
+);
